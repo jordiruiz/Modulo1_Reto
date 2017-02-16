ea48dda054d259c270ff56b2b518bb678868540b
jordiruizperez <jordi.ruiz@takeu.es>
2017-02-16 09:24:01 +0100

    add project

---
+++ Modulo1_Reto/Modulo1_Reto/Views/MainView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Modulo1_Reto.Views
+{
+    public partial class MainView : ContentPage
+    {
+        int count = 1;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            MyButton.Clicked += (sender, args) =>
+            {
+                MyLabel.Text = $"Has pulsado {count++} veces!";
+            };
+        }
+    }
+}
